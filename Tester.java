62e2f0f9e7653e6e0da69059916a0ca306e4d0c3
jzhong10 <junzhong2003@gmail.com>
2020-10-29 15:32:58 -0400

    Implemented and tested Point class

---
+++ Tester.java
@@ -0,0 +1,10 @@
+public class Tester {
+    public static void main(String[] args) {
+      Point p1 = new Point(1, 2);
+      Point p2 = new Point(-100, 23);
+      System.out.println(p1.getX());
+      System.out.println(p1.getY());
+      System.out.println(p2.getX());
+      System.out.println(p2.getY());
+    }
+}
